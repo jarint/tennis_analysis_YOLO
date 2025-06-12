ae4b4ae8f2cbe8e672747b3387c8136165017c8e
jarint <jarin.thundathil@ucalgary.ca>
2025-06-11 23:18:12 -0600

    minor changes

--- yolo_inference.py
+++ yolo_inference.py
@@ -3,4 +3,8 @@ from ultralytics import YOLO
 model = YOLO('yolov8x')
 
 # inference - simplistic working YOLO
-model.predict('input_videos/image.png', save = True)
+result = model.predict('input_videos/image.png', save = True)
+print(result)
+print("boxes: ")
+for box in result[0].boxes:
+    print(box)
